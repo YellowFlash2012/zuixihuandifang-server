26fc210f61dd65e348e01b65a9de23eebad3eb32
YellowFlash2012 <cyclop9@hotmail.com>
2022-01-13 08:04:20 +0100

    get all users route updated and connected to the db

--- routes/users.js
+++ routes/users.js
@@ -32,8 +32,18 @@ const users = [
 ]
 
 // ****get all users****
-router.get("/", (req, res, next) => {
-    res.json({ users: users });
+router.get("/", async (req, res, next) => {
+    let users;
+
+    try {
+        users = await Users.find({}, '-password');
+    } catch (err) {
+        const error = new HttpError("Fetching users failed, please try again later", 500);
+
+        return next(error);
+    }
+    
+    res.json({ users: users.map(user => user.toObject({ getters: true })) });
 })
 
 // ****users signup****
