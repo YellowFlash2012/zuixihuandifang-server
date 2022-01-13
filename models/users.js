72c46a832bf3e790d4a1aa777f8463cf70fcf0d6
YellowFlash2012 <cyclop9@hotmail.com>
2022-01-13 06:27:01 +0100

    users model schema created with validation from mongoose-unique-validator set and running

--- models/users.js
+++ models/users.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
+    name: { type: String, required: true },
+
+    email: { type: String, required: true, unique: true },
+    
     image: { type: String, required: true },
-    address: { type: String, required: true },
-    location: {
-        lat: { type: Number, required: true },
-        lng: { type: Number, required: true },
-    },
-    creator: { type: String, required: true },
+
+    password: { type: String, required: true, minlength: 13 },
+    
+    places: { type: String, required: true };
     
 });
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Users", userSchema);
